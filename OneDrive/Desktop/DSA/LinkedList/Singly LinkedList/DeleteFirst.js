b54d565979d228d3af20935fe82038dbeec6e4ae
Rashid <crashid966@gmail.com>
2025-05-24 12:05:44 +0530

    added the delete last node of a linked list

--- OneDrive/Desktop/DSA/LinkedList/Singly LinkedList/DeleteFirst.js
+++ OneDrive/Desktop/DSA/LinkedList/Singly LinkedList/DeleteFirst.js
@@ -11,38 +11,33 @@ function addNode(data){
         head = newNode;
         return
     }
-    let current = head
+    let current = head;
     while(current.next !== null){
-        current = current.next;
+        current = current.next
     }
     current.next = newNode;
 }
-function deleteLast(){
-    if(head === null)return;
-    if(head.next === null){
-        head = null;
-        return
-    }
-    let current = head;
-    while(current.next.next !== null){
-        current = current.next
-    }
-    current.next = null
+function deleteNode(){
+    if(head === null) return
+    head = head.next
 }
-
 function display(){
     if(head === null){
-        console.log("Linked list is empty")
+        console.log('Linked list is empty')
+        return
     }
     let current = head;
     while(current !== null){
         console.log(current.data)
-        current = current.next
+        current = current.next;
     }
 }
-addNode(12)
-addNode(22)
-addNode(32)
-addNode(42)
-deleteLast()
+    
+addNode(32);
+addNode(12);
+addNode(22);
+addNode(42);
+addNode(52);
+addNode(62);
+deleteNode();
 display()
