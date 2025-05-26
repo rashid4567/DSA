d6707707be34db6a68fd43f3513e4c08865d9573
Rashid <crashid966@gmail.com>
2025-05-26 18:46:01 +0530

    added the doubly linked list

---
+++ OneDrive/Desktop/DSA/LinkedList/double LinkedList/addFirst.js
@@ -0,0 +1,58 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.prev = null
+        this.next = null;
+    }
+}
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+    addNode(data){
+        let newNode = new Node(data);
+        if(this.head === null){
+            this.head = this.tail = newNode;
+            return
+        }
+        let current = this.head
+        while(current.next !== null){
+            current = current.next
+        }
+        current.next = newNode;
+    }
+    addFirst(data){
+        let newNode = new Node(data);
+        if(this.head === null){
+            this.head = this.tail = newNode;
+            return
+        }
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode
+    }
+    display(){
+        if(this.head === null){
+            console.log("Linked list is empty");
+            return
+        }
+        let current = this.head;
+        let out = "head -->"
+        while(current !== null){
+            out += ` [${current.data}] <-->`
+            current = current.next;
+        }
+        out += " null"
+        console.log(out)
+    }
+}
+
+let DDL = new DoublyLinkedList();
+DDL.addNode(25)
+DDL.addNode(24)
+DDL.addNode(23)
+DDL.addNode(22)
+DDL.addNode(21)
+DDL.addFirst(20);
+DDL.display()
