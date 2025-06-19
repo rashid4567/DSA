bb51ecbcc0785996188c86c55b61e3cd2d48ffe3
Rashid <crashid966@gmail.com>
2025-06-19 20:34:41 +0530

    added some bubble sort

---
+++ OneDrive/Desktop/DSA/sorting/bubbleSort/SortStringsAlphabetically.js
@@ -0,0 +1,16 @@
+function bubbleSortStrings(arr){
+    let swapped;
+    do{
+        swapped = false;
+        for(let i=0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+                swapped = true;
+            }
+        }
+    }while(swapped);
+}
+
+let arr = ["welcome","to","every","one"]
+bubbleSortStrings(arr);
+console.log(arr)
