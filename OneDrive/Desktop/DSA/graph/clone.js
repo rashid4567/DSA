c1c7196dd5826b6383d5004ce734c5967308e112
Rashid <crashid966@gmail.com>
2025-07-10 19:37:44 +0530

    added

--- OneDrive/Desktop/DSA/graph/clone.js
+++ OneDrive/Desktop/DSA/graph/clone.js
@@ -1,53 +1,49 @@
 class Graph{
     constructor(isDirected = false){
         this.adjList = {};
-        this.isDirected = isDirected;
+        this.isDirected = isDirected
     }
     addVertex(vertex){
         if(!this.adjList[vertex]){
             this.adjList[vertex] = new Set()
         }
-          for(let v in this.adjList){
-            if(!this.isDirected){
-                this.adjList[v].add(vertex)
-            }
-        }
     }
-    addEdges(v1, v2){
+    addEdges(v1,v2){
         this.addVertex(v1);
         this.addVertex(v2);
-
-        this.adjList[v1].add(v2)
-        if(this.adjList[v1]){
+        this.adjList[v1].add(v2);
+        if(!this.isDirected){
             this.adjList[v2].add(v1)
         }
     }
     clone(){
-        let newGraph = new Graph(this.isDirected = false);
+        let newGraph = new Graph(this.isDirected);
         for(let node in this.adjList){
-            this.addVertex(node)
+            newGraph.addVertex(node);
         }
-
+        
         for(let node in this.adjList){
             for(let n of this.adjList[node]){
                 if(this.isDirected || node < n){
-                   this.addEdges(node, n)   
+                    newGraph.addEdges(node, n)
                 }
             }
         }
-        return newGraph;
+        return newGraph
     }
     print(){
-        for(let n in this.adjList){
-            console.log(`${v}-> ${[...this.adjList[v]].join(", ")}`)
+        for(let vertex in this.adjList){
+            console.log(`${vertex}->${[...this.adjList[vertex]].join(", ")}`)
         }
     }
 }
+
 
 let g = new Graph(false);
 g.addEdges("A", "B");
 g.addEdges("A", "C");
 g.addEdges("B", "C");
 g.addEdges("C", "A");
-const clone = g.clone()
-clone.print()
+
+const clone = g.clone();
+clone.print();
